b00dbfd71e4e2fd6d132c93627b16b1499e84f6b
linyisonger <752957127@qq.com>
2021-05-15 21:22:58 +0800

    - 更新测试代码

--- Yu.Toolkit.RegionCode.Test/RegionCodeTest.cs
+++ Yu.Toolkit.RegionCode.Test/RegionCodeTest.cs
@@ -17,10 +17,6 @@ namespace Yu.Toolkit.Test
             Assert.True(RegionCode.Provinces.Count > 0);
             Assert.True("湖北省" == RegionCode.GetProvinceNameByCode("420000"));
             Assert.True("420000" == RegionCode.GetProvinceCodeByName("湖北省"));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetProvinceNameByCode("420100")));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetProvinceNameByCode("420106")));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetProvinceCodeByName("武汉市")));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetProvinceCodeByName("武昌区")));
         }
         [Fact(DisplayName = "市代码测试")]
         public void CityTest()
@@ -28,10 +24,6 @@ namespace Yu.Toolkit.Test
             Assert.True(RegionCode.Cities.Count > 0);
             Assert.True("武汉市" == RegionCode.GetCityNameByCode("420100"));
             Assert.True("420100" == RegionCode.GetCityCodeByName("武汉市"));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetCityNameByCode("420000")));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetCityNameByCode("420106")));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetCityCodeByName("湖北省")));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetCityCodeByName("武昌区")));
         }
         [Fact(DisplayName = "区代码测试")]
         public void AreaTest()
@@ -39,10 +31,6 @@ namespace Yu.Toolkit.Test
             Assert.True(RegionCode.Areas.Count > 0);
             Assert.True("武昌区" == RegionCode.GetAreaNameByCode("420106"));
             Assert.True("420106" == RegionCode.GetAreaCodeByName("武昌区"));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetAreaNameByCode("420000")));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetAreaNameByCode("420100")));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetAreaCodeByName("湖北省")));
-            Assert.True(string.IsNullOrEmpty(RegionCode.GetAreaCodeByName("武汉市")));
         }
 
     }
